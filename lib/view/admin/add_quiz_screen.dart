e3d654d04c93e9ae4a1a99d5271e33a85b037740
janak <shahj52@gmail.com>
2026-02-11 23:07:37 +0530

    11/2/26 11:07

--- lib/view/admin/add_quiz_screen.dart
+++ lib/view/admin/add_quiz_screen.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:smart_quiz/model/category.dart';
+import 'package:smart_quiz/model/question.dart';
+import 'package:smart_quiz/model/quiz.dart';
 import 'package:smart_quiz/theme/theme.dart';
 
 class AddQuizScreen extends StatefulWidget {
@@ -68,13 +71,146 @@ class _AddQuizScreenState extends State<AddQuizScreen> {
     });
   }
 
-  //2:24:30
+  void _removeQuestion(int index) {
+    setState(() {
+      _questionsItems[index].dispose();
+      _questionsItems.removeAt(index);
+    });
+  }
+
+  Future<void> _saveQuiz() async {
+    if (!_fromKey.currentState!.validate()) {
+      return;
+    }
+    if (_selectedCategoryId == null) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Please Select Catgory")));
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      final questions = _questionsItems
+          .map(
+            (item) => Question(
+              text: item.questionController.text.trim(),
+              options: item.optionControllers
+                  .map((e) => e.text.trim())
+                  .toList(),
+              correctOptionIndex: item.correctOptionIndex,
+            ),
+          )
+          .toList();
+
+      await _firestore
+          .collection("quizzes")
+          .doc()
+          .set(
+            Quiz(
+              id: _firestore.collection("quizzes").doc().id,
+              title: _titleController.text.trim(),
+              categoryId: _selectedCategoryId!,
+              timeLimit: int.parse(_timeLimitController.text),
+              questions: questions,
+              createdAt: DateTime.now(),
+            ).toMap(),
+          );
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Quiz added Successfully")));
+      Navigator.pop(context);
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            "Failed to add quiz",
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: AppTheme.secondaryColor,
+        ),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppTheme.backgroundColor,
-        title: Text("Add Quiz Question"),
+        title: Text("Add Quiz", style: TextStyle(fontWeight: FontWeight.bold)),
+      ),
+      body: Form(
+        key: _fromKey,
+        child: ListView(
+          padding: EdgeInsets.all(20),
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Quiz Detail",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: AppTheme.textPrimaryColor,
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _titleController,
+                  decoration: InputDecoration(
+                    labelText: "Quiz Title",
+                    hintText: "Enter quiz Title",
+                    prefixIcon: Icon(Icons.title, color: AppTheme.primaryColor),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter quiz title";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16),
+                if (widget.categoryId == null)
+                  StreamBuilder<QuerySnapshot>(
+                    stream: _firestore
+                        .collection('categories')
+                        .orderBy('name')
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return Text("Error");
+                      }
+                      if (!snapshot.hasData) {
+                        return Center(
+                          child: CircularProgressIndicator(
+                            color: AppTheme.primaryColor,
+                          ),
+                        );
+                      }
+                      final categories = snapshot.data!.docs
+                          .map(
+                            (doc) => Category.fromMap(
+                              doc.id,
+                              doc.data() as Map<String, dynamic>,
+                            ),
+                          )
+                          .toList();
+
+                      //2:33:00
+                    },
+                  ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
